2267628fde86d3dcf205e23906688c73fa8cb9f2
AlyssaJoyFlores <114978911+AlyssaJoyFlores@users.noreply.github.com>
2024-02-06 12:12:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ server.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ['strict-dynamic', 'nonce-rAnd0m', 'https:'],,
+      scriptSrc: ['strict-dynamic', 'nonce-rAnd0m', 'https:'],
       styleSrc: ["'self'", "https://fonts.googleapis.com", "'unsafe-inline'"],
       imgSrc: ["'self'", "https://paucs.store", "https://res.cloudinary.com", "https://ui-avatars.com/api"],
       fontSrc: ["'self'", 'https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
