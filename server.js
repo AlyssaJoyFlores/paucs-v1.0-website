eb14316ca9af9ed3d0dd0b93c9cd4947cb6f2f32
AlyssaJoyFlores <114978911+AlyssaJoyFlores@users.noreply.github.com>
2024-02-06 10:42:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ server.use(
         fontSrc: ["'self'", 'https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
         objectSrc: ["'none'"], 
         frameAncestors: ["'self'", "https://paucs.store"], 
-        connectSrc: ["'self'", "https://paucs.store", "https://res.cloudinary.com", "https://ui-avatars.com/api", "https://www.google.com", "https://www.google.com/recaptcha/api/siteverify"], 
+        connectSrc: ["'self'", "https://paucs.store", "https://res.cloudinary.com", "https://ui-avatars.com/api", "https://www.google.com/recaptcha"], 
         mediaSrc: ["'self'", "https://paucs.store"], 
         formAction: ["'self'", "https://paucs.store"], 
         requireTrustedTypesFor: ['script-src']
