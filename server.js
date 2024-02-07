51a4166f9023cd4e539a60179b452ceb185556ea
AlyssaJoyFlores <114978911+AlyssaJoyFlores@users.noreply.github.com>
2024-02-07 14:47:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -147,8 +147,7 @@ server.use(
         connectSrc: ["'self'", "https://paucs.store", "https://res.cloudinary.com", "https://ui-avatars.com/api",  "https://www.google.com/recaptcha/"], 
         frameSrc: ["'self'", "https://www.google.com"],
         mediaSrc: ["'self'", "https://paucs.store"],
-        formAction: ["'self'", "https://paucs.store"],
-        'require-trusted-types-for': ["'script'"] 
+        formAction: ["'self'", "https://paucs.store"]
       },
     }),
     helmet.frameguard({ action: "deny" }),
